41d9761e53437bb5ecef8c3732bc0bb7fcd6e2d1
qinx0 <qinx.developer@gmail.com>
2025-02-25 10:21:41 +0100

    e

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import engine_main
 import engine
+import engine_io
+import engine_draw
+from engine_draw import Color
+import engine_resources
+
+blocks = engine_resources.
 
 while True:
     if engine.tick():
