8c4a1b49b5165bf3d446f2515bee396e37da0cf9
qinx0 <qinx.developer@gmail.com>
2025-03-07 21:40:23 +0100

    anada one

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import engine_draw
 from engine_draw import Color
 import engine_resources
 
-blocks = engine_resources.
+blockTex = engine_resources.TextureResource("Images/Tiny Blocks_1.0/tinyBlocks_NoLine.bmp")
+playerTex = engine_resources.TextureResource("Images/Small-8-Direction-Characters_by_AxulArt.bmp")
 
 while True:
     if engine.tick():
