dd15440582503d4e11100cfe868ff5f34cab63ff
qinx0 <qinx.developer@gmail.com>
2025-03-30 21:15:28 +0200

    Changed how player s created and movement and added ISOMETRC TILEMAPPPPPPPP

--- main.py
+++ main.py
@@ -1,70 +1,111 @@
-import engine_main  # type: ignore
-import engine  # type: ignore
-import engine_io  # type: ignore
-import engine_draw  # type: ignore
-import engine_resources  # type: ignore
+import engine_main  
+import engine  
+import engine_io  
+import engine_draw  
+import engine_resources  
 import random
-from engine_nodes import Sprite2DNode, CameraNode, Text2DNode, Rectangle2DNode  # type: ignore
-from engine_math import Vector2  # type: ignore
-from engine_draw import Color  # type: ignore
+from engine_nodes import Sprite2DNode, CameraNode, Text2DNode, Rectangle2DNode, EmptyNode  
+from engine_math import Vector2  
+from engine_draw import Color  
 
 def RGB888to565(c):
     return ((c[0] >> 3) << 11) | ((c[1] >> 2) << 5) | c[2] >> 3
 
 doMovement = True
-# buttons = ["UP","DOWN","LEFT","RIGHT","B","A","LB","RB","MENU"]
-
 blocksTex = engine_resources.TextureResource("Images/blocks.bmp")
 playerTex = engine_resources.TextureResource("Images/player.bmp")
-player = Sprite2DNode(Vector2(0, -10), playerTex, Color(1.0, 0.0, 1.0), frame_count_x=4, frame_count_y=3, layer=1)
-player.playing = False
-# map = Sprite2DNode(Vector2(0,0), blocksTex, Color(1.0,0.0,1.0), frame_count_x=7, frame_count_y=7, layer=0)
-# map.playing = False
-camera = CameraNode()
-player.add_child(camera)
 
+def generateMap(image_path):
+    TILE_WIDTH = 18   
+    TILE_HEIGHT = 9   
+    TILE_ROWS = 7     
+    TILE_COLS = 7     
+    
+    texture_id = engine_resources.TextureResource(image_path)
 
-def generateMap():
-    for x in range(7):
-        for y in range(7):            
-            if x < 4 and y < 4:
-                bTp = [random.randint(0, 7), random.randint(0, 7)]
-                bp = Vector2(int(x * -18), int(y * -18))
-                b = Sprite2DNode(bp, blocksTex, Color(1.0, 0.0, 1.0), frame_count_x=7, frame_count_y=7, layer=0)
-                b.playing = False
-                b.frame_current_x = bTp[0]
-                b.frame_current_y = bTp[1]
-                print(f"Block created: position: ({bp.x},{bp.y}) ; block: ({bTp[0]},{bTp[1]})")
-            elif x < 4 and y >= 4:
-                bTp = [random.randint(0, 7), random.randint(0, 7)]
-                bp = Vector2(int(x * -18), int(y * 18 // 2))
-                b = Sprite2DNode(bp, blocksTex, Color(1.0, 0.0, 1.0), frame_count_x=7, frame_count_y=7, layer=0)
-                b.playing = False
-                b.frame_current_x = bTp[0]
-                b.frame_current_y = bTp[1]
-                print(f"Block created: position: ({bp.x},{bp.y}) ; block: ({bTp[0]},{bTp[1]})")
-            elif y < 4 and x >= 4:
-                bTp = [random.randint(0, 7), random.randint(0, 7)]
-                bp = Vector2(int(x * 18 // 2), int(y * -18))
-                b = Sprite2DNode(bp, blocksTex, Color(1.0, 0.0, 1.0), frame_count_x=7, frame_count_y=7, layer=0)
-                b.playing = False
-                b.frame_current_x = bTp[0]
-                b.frame_current_y = bTp[1]
-                print(f"Block created: position: ({bp.x},{bp.y}) ; block: ({bTp[0]},{bTp[1]})")
+    tilemap = EmptyNode()
+    tilemap.layer = 0  
+    
+    start_x = -((TILE_COLS * TILE_WIDTH) // 2)
+    start_y = -((TILE_ROWS * TILE_HEIGHT) // 2)
+    
+    for row in range(TILE_ROWS):
+        for col in range(TILE_COLS):
+            tile = Sprite2DNode()
+            tile.texture = texture_id
+            tile.layer = 0
+            tile.playing = False
+            tile.transparent_color = Color(1.0,0.0,1.0)
+            
+            tile.frame_count_x = TILE_ROWS
+            tile.frame_count_y = TILE_COLS
+            tile.frame_current_x = row
+            tile.frame_current_y = col
+            
+            tile_x = start_x + (col - row) * (TILE_WIDTH // 2)
+            tile_y = start_y + (col + row) * (TILE_HEIGHT // 2)
 
+            tile.position.x = tile_x
+            tile.position.y = tile_y
+            
+            tilemap.add_child(tile)
+    
+    rootMap = EmptyNode()
+    rootMap.add_child(tilemap)
 
-map = generateMap()
+map = generateMap("Images/blocks.bmp")
 
-def movement():
-    if engine_io.UP.is_just_pressed:
-        player.position.y -= 18
-    elif engine_io.DOWN.is_just_pressed:
-        player.position.y += 18
-    elif engine_io.LEFT.is_just_pressed:
-        player.position.x -= 18
-    elif engine_io.RIGHT.is_just_pressed:
-        player.position.x += 18
+def createPlayer():
+    TILE_WIDTH = 18
+    TILE_HEIGHT = 9 
+    
+    player_texture = engine_resources.TextureResource("Images/player.bmp")
+    
+    player = Sprite2DNode()
+    player.texture = player_texture
+    player.layer = 1  
+    player.playing = False
+    player.transparent_color = Color(1.0,0.0,1.0)
+    camera = CameraNode()
+    player.add_child(camera)
+    player.frame_count_x = 4
+    player.frame_count_y = 3
+    
+    start_col, start_row = 0, 0  
+    player.position.x = (start_col - start_row) * (TILE_WIDTH // 2) + 1
+    player.position.y = (start_col + start_row) * (TILE_HEIGHT // 2) + 3
+    
+    global playercol 
+    playercol = start_col
+    global playerrow
+    playerrow = start_row
+    return player
+
+def updatePlayer():
+    global playercol
+    global playerrow
+    TILE_WIDTH = 18   
+    TILE_HEIGHT = 9
+    move_col = 0
+    move_row = 0
+
+    if engine_io.UP.is_just_pressed:  
+        move_row -= 1
+    if engine_io.DOWN.is_just_pressed:  
+        move_row += 1
+    if engine_io.LEFT.is_just_pressed:  
+        move_col -= 1
+    if engine_io.RIGHT.is_just_pressed:  
+        move_col += 1
+    
+    playercol += move_col
+    playerrow += move_row
+    
+    player.position.x = (playercol - playerrow) * (TILE_WIDTH // 2) + 1
+    player.position.y = (playercol + playerrow) * (TILE_HEIGHT // 2) + 3
+
+player = createPlayer()
 
 while True:
     if engine.tick():
-        movement()
+        updatePlayer()
