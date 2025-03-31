4928ea6785b85094fb42a33076faf42a14de6f27
qinx0 <qinx.developer@gmail.com>
2025-03-31 19:53:12 +0200

    Git sucks az oh my god it stopped working for an hour

--- main.py
+++ main.py
@@ -11,17 +11,24 @@ from engine_draw import Color
 def RGB888to565(c):
     return ((c[0] >> 3) << 11) | ((c[1] >> 2) << 5) | c[2] >> 3
 
-tA = False
-direction = 1
+aFrame = 0
+aFinished = True
+incrementAFrame = False
+frameDelayCounter = 0
+frameDelayMax = 45
 doMovement = True
+
+direction = 1
 blocksTex = engine_resources.TextureResource("Images/blocks.bmp")
 playerTex = engine_resources.TextureResource("Images/player.bmp")
 
 def generateMap(image_path):
-    TILE_WIDTH = 18   
+    TILE_WIDTH = 16
     TILE_HEIGHT = 9   
     TILE_ROWS = 7     
-    TILE_COLS = 7     
+    TILE_COLS = 7 
+    TILEMAP_ROWS = 14
+    TILEMAP_COLS = 14    
     
     texture_id = engine_resources.TextureResource(image_path)
 
@@ -29,11 +36,11 @@ def generateMap(image_path):
     tilemap.layer = 0  
     tilemap.position = Vector2(0,0)
     
-    start_x = -((TILE_COLS * TILE_WIDTH) // 2)
-    start_y = -((TILE_ROWS * TILE_HEIGHT) // 2)
+    start_x = -((TILEMAP_COLS * TILE_WIDTH) // 2)
+    start_y = -((TILEMAP_ROWS * TILE_HEIGHT) // 2)
     
-    for row in range(TILE_ROWS):
-        for col in range(TILE_COLS):
+    for row in range(TILEMAP_ROWS):
+        for col in range(TILEMAP_COLS):
             tile = Sprite2DNode()
             tile.texture = texture_id
             tile.layer = 0
@@ -42,8 +49,8 @@ def generateMap(image_path):
             
             tile.frame_count_x = TILE_ROWS
             tile.frame_count_y = TILE_COLS
-            tile.frame_current_x = row
-            tile.frame_current_y = col
+            tile.frame_current_x = random.randint(0,7)
+            tile.frame_current_y = random.randint(0,7)
             
             tile_x = start_x + (col - row) * (TILE_WIDTH // 2)
             tile_y = start_y + (col + row) * (TILE_HEIGHT // 2)
@@ -57,7 +64,7 @@ def generateMap(image_path):
     rootMap.add_child(tilemap)
 
 def createPlayer():
-    TILE_WIDTH = 18
+    TILE_WIDTH = 16
     TILE_HEIGHT = 9 
     
     player_texture = engine_resources.TextureResource("Images/player.bmp")
@@ -74,8 +81,7 @@ def createPlayer():
     
     start_col, start_row = 0, 0  
     player.position.x = (start_col - start_row) * (TILE_WIDTH // 2) + 1
-    player.position.y = (start_col + start_row) * (TILE_HEIGHT // 2) + 3
-    
+    player.position.y = (start_col + start_row) * (TILE_HEIGHT // 2) + 5
     global playercol 
     playercol = start_col
     global playerrow
@@ -83,44 +89,69 @@ def createPlayer():
     return player
 
 def updatePlayer():
-    if not doMovement:
+    global playercol, playerrow, direction, aFrame, aFinished, incrementAFrame, doMovement
+
+    if not doMovement:  
         return
-    global playercol
-    global playerrow
-    global direction
-    TILE_WIDTH = 18   
+
+    TILE_WIDTH = 16
     TILE_HEIGHT = 9
     move_col = 0
     move_row = 0
 
+    
     if engine_io.UP.is_just_pressed:
-        direction = 1
+        direction = 0  
         move_row -= 1
-        print(f"Direction: {direction}")
-    if engine_io.DOWN.is_just_pressed:
-        direction = 2
+    elif engine_io.DOWN.is_just_pressed:
+        direction = 2  
         move_row += 1
-        print(f"Direction: {direction}")
-    if engine_io.LEFT.is_just_pressed:
-        direction = 3
+    elif engine_io.LEFT.is_just_pressed:
+        direction = 3  
         move_col -= 1
-        print(f"Direction: {direction}")
-    if engine_io.RIGHT.is_just_pressed:
-        direction = 4
+    elif engine_io.RIGHT.is_just_pressed:
+        direction = 1  
         move_col += 1
-        print(f"Direction: {direction}")
+    else:
+        return  
+
+    
+    doMovement = False
+    aFinished = False
+    incrementAFrame = True
+
     
     playercol += move_col
     playerrow += move_row
-    
     player.position.x = (playercol - playerrow) * (TILE_WIDTH // 2) + 1
-    player.position.y = (playercol + playerrow) * (TILE_HEIGHT // 2) + 3
+    player.position.y = (playercol + playerrow) * (TILE_HEIGHT // 2) + 5
 
-def animate(node: Sprite2DNode, aNum: int):
-    node.playing = True
+def animate(node: Sprite2DNode, animationNum: int, steps: int, framedelay: int):
+    global aFrame, aFinished, incrementAFrame, frameDelayCounter, doMovement
 
-def animStop(node):
-    node.playing = False
+    if aFinished:
+        return  
+
+    
+    if frameDelayCounter < framedelay:
+        frameDelayCounter += 1
+        return
+    frameDelayCounter = 0  
+
+    
+    if aFrame < steps:
+        node.frame_current_x = animationNum  
+        node.frame_current_y = aFrame - 1  
+        aFrame += 1
+    else:
+        aFrame = 0
+        aFinished = True
+        incrementAFrame = False
+        node.frame_current_x, node.frame_current_y = animationNum, aFrame+1  
+
+        
+        doMovement = True  
+
 
 player = createPlayer()
 map = generateMap("Images/blocks.bmp")
@@ -128,9 +159,6 @@ map = generateMap("Images/blocks.bmp")
 while True:
     if engine.tick():
         updatePlayer()
+        animate(player, direction, 3, frameDelayMax)
         if engine_io.A.is_just_pressed:
-            doMovement = not doMovement
-        if engine_io.B.is_just_pressed:
-            if tA: tA = not tA; animStop(player); print("Started animating")
-            else: tA = not tA; animate(player, 1); print("Stopped animating")
-            
+            doMovement = not doMovement         
